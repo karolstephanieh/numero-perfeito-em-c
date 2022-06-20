ac062dc030d3210dafea8771adacae17665ce624
Karoline Stephanie <101149956+karolstephanieh@users.noreply.github.com>
2022-06-19 21:18:24 -0400

    Create usando-for-e-if.c

---
+++ usando-for-e-if.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+	
+	int n, i, soma;
+	
+	printf ("Insira um número:");
+	scanf ("%d", &n);
+	
+	soma = 0;
+		
+	for (i = 1; i <= n/2; i++){
+		if (n%i == 0);
+		soma += i;
+	}
+	
+	if (soma == n){
+		printf ("O número inserido é perfeito");
+	} else {
+		printf ("O número inserido não é perfeito");
+	}
+	
+	return 0;
+}
+
+// lembrando que sem o locale, o programa não vai ler as acentuações, aqui está apenas para demonstração didática
